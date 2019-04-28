eeccb422606ae1a791b1e862fe3d98b63936c46d
Sam Reeve <sam.reeve95@gmail.com>
2019-04-28 02:20:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,39 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+window.addEventListener('DOMContentLoaded', init, false);
+
+function init(event) 
+{
+ 
+  var p = document.querySelectorAll("div")[1];
+ 
+  p.addEventListener('click', clicker);
+  p.addEventListener('mouseover', hover);
+  p.addEventListener('mouseout', unhover);
+}
+
+function clicker(e)
+{
+  if (e.target.className.search("blue") === -1)
+  {
+    e.target.className = String.trim(e.target.className + " blue");
+  }
+  else
+  {
+    e.target.className = e.target.className.replace("blue","red");
+  }
+}
+
+function hover(e)
+{
+  var str = e.target.className;
+  if (str.search("hover") === -1)
+    e.target.className = String.trim(e.target.className + " hover");
+}
+
+function unhover(e)
+{
+  console.log("unhover");
+  e.target.className = e.target.className.replace(" hover","");
+}
