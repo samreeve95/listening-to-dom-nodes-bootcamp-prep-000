64e85ab73a161a91e9078bae0f751063408785c2
Sam Reeve <sam.reeve95@gmail.com>
2019-04-28 02:57:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -15,7 +15,11 @@ function init(event)
   var input = document.querySelector('input');
 
   input.addEventListener('keydown', function(e) {
-    console.log(e.key);
+    if (e.key === "g") {
+    return e.preventDefault()
+  } else {
+    console.log(e.key)
+  }
   });
 }
 
